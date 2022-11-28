0e06be65ac191367a1258fbefbb55fce6873f8ed
Thomas6A <thomas.baudrin@outlook.fr>
2022-11-28 10:28:55 +0100

    exo

---
+++ src/exolist/Exo1.java
@@ -0,0 +1,17 @@
+package exolist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exo1 {
+	public static void main(String[] args) {
+		List<String> languages = new ArrayList<String>();
+		languages.add("PHP");
+		languages.add("Java");
+		languages.add("C++");
+		languages.add("Python");
+		
+		System.out.println(languages);
+	}
+
+}
